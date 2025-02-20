22396126028e9dea1af9e635596bd399a9edc7ae
Ker1star <cockguy1337@gmail.com>
2025-02-20 18:31:52 +0300

    f

--- api/bot.py
+++ api/bot.py
@@ -55,7 +55,7 @@ async def telegram_webhook(request: Request, token: str):
         data = await request.json()
         logger.debug(f"Data received: {data}")
         update = Update(**data)
-        await dp.feed_update(update)  # Используем для обработки обновлений
+        await dp.feed_update(bot, update)  # Используем для обработки обновлений
         return {"ok": True}
     except Exception as e:
         logger.error(f"Error processing webhook: {e}")
